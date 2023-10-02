2a1a32aeefa1ff0ceb12ad5a31eda896295d95d6
Numan Şenlik <nmnsnlk@gmail.com>
2023-10-02 22:42:19 +0300

    firt commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var message = "Hello World";
+console.log(message);
+var h1 = document.createElement('h1');
+h1.textContent = message;
+document.body.appendChild(h1);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let message : string = "Hello World"
+console.log(message);
+
+const h1 = document.createElement('h1')
+h1.textContent = message
+document.body.appendChild(h1)
