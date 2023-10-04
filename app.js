8af941cb15712b9d7a8592d653c072edff1835c8
Numan Şenlik <nmnsnlk@gmail.com>
2023-10-05 02:22:48 +0300

    Final Commit

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-var message = "Hello World";
-console.log(message);
-var h1 = document.createElement('h1');
-h1.textContent = message;
-document.body.appendChild(h1);
+// let message : string = "Hello World"
+// console.log(message);
+// const h1 = document.createElement('h1')
+// h1.textContent = message
+// document.body.appendChild(h1)
+/*
+// Geliştirme aşamasında değişkenlerin tipini belirtirken hata yapmamızı engeller.
+let reversedText = (text: string) => {
+  return text.split('').reverse().join('')
+}
+
+console.log(reversedText('merhaba'));
+*/
+// interface Product {
+//   name: string;
+//   price: number;
+//   test: boolean;
+// }
+// function getProduct(name: string, price: number, test: boolean): Product {
+//   return {
+//     name: name,
+//     price: price,
+//     test: test,
+//   };
+// }
+// let showProduct = () => {
+//   console.log(getProduct('asddf', 200, true));
+// };
+// showProduct();
+//! Baştan değişkene değer tanımlamak direkt değişkene değer tanımlamakla aynı
+// let sayi = 0;
+// // sayi  ='tayfun'
+// function setCounter(max: 100) {
+//   return max
+// }
+// setCounter(10)
+// TypeScript Değikenlerin konumunu kullanarak değişken tiplerini otamatik olarak algılar
+// document.addEventListener('click'function (event) {
+//   console.log(event.button);
+// })
--- app.ts
+++ app.ts
@@ -1,6 +1,52 @@
-let message : string = "Hello World"
-console.log(message);
+// let message : string = "Hello World"
+// console.log(message);
 
-const h1 = document.createElement('h1')
-h1.textContent = message
-document.body.appendChild(h1)
+// const h1 = document.createElement('h1')
+// h1.textContent = message
+// document.body.appendChild(h1)
+
+
+/*
+// Geliştirme aşamasında değişkenlerin tipini belirtirken hata yapmamızı engeller.
+let reversedText = (text: string) => {
+  return text.split('').reverse().join('')
+}
+
+console.log(reversedText('merhaba'));
+*/
+// interface Product {
+//   name: string;
+//   price: number;
+//   test: boolean;
+// }
+
+// function getProduct(name: string, price: number, test: boolean): Product {
+//   return {
+//     name: name,
+//     price: price,
+//     test: test,
+//   };
+// }
+
+// let showProduct = () => {
+//   console.log(getProduct('asddf', 200, true));
+// };
+
+// showProduct();
+//! Baştan değişkene değer tanımlamak direkt değişkene değer tanımlamakla aynı
+// let sayi = 0;
+// // sayi  ='tayfun'
+
+// function setCounter(max: 100) {
+//   return max
+// }
+// setCounter(10)
+// TypeScript tip Çıkarımı Değikenlerin konumunu kullanarak değişken tiplerini otamatik olarak algılar
+// document.addEventListener('click'function (event) {
+//   console.log(event.button);
+  
+// })
+// Özet
+// Tip Çıkarımları, bir değişken tanımladığınızda, parametreye vasayılan değer belirlediğinizde, fonsiyonda geriye bir değer dönerken uygulanır.
+// TypeScript, en uygun tipi bulmak için en yaygın tip algoritmasını kullanarak çıkarım yapar.
+// Ayrıca bağlamsal yazma ile değişkenlerin konumlarını kullanarak çıkarım yapar.
